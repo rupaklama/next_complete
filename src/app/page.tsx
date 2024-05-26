cacbe98319453eb626424f875d18a75237f0c7b7
Rupak Lama <rupaklamadev@gmail.com>
2024-05-26 12:55:12 -0400

    fetch data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return <div>Home Page</div>;
+import { db } from "@/db";
+
+const HomePage = async () => {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map(snippet => {
+    return (
+      <div key={snippet.id} className="border p-4 m-4 overflow-hidden">
+        <h3 className="font-bold">{snippet.title}</h3>
+        <pre>{snippet.code}</pre>
+      </div>
+    );
+  });
+
+  return <div>{renderedSnippets}</div>;
 };
 
 export default HomePage;
